ba8b050246383a514320ea7e9c81b214ce91c7db
zaakiraza <raaza.zaakir@gmail.com>
2025-07-30 12:30:53 +0500

    vercel update

--- FrontEnd/User/vite.config.js
+++ FrontEnd/User/vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Vercel will look for this output directory
-  }
 })
