6eafafb408c3d9aac588995000457b7f0da96235
zaakiraza <raaza.zaakir@gmail.com>
2025-10-19 04:30:33 +0500

    frontend

--- FrontEnd/src/config/api.js
+++ FrontEnd/src/config/api.js
@@ -1,9 +1,9 @@
 // API Configuration
 export const API_CONFIG = {
-  BASE_URL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3001/api",
-  // BASE_URL:
-  //   import.meta.env.VITE_API_BASE_URL ||
-  //   "https://hackathonbackend-omega.vercel.app/api",
+  // BASE_URL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3001/api",
+  BASE_URL:
+    import.meta.env.VITE_API_BASE_URL ||
+    "https://hackathonbackend-omega.vercel.app/api",
   ENDPOINTS: {
     // Authentication endpoints
     LOGIN: "/auth/login",
